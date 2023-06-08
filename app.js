ec07b36b398bd3d00ebff9eaf621ee6b990990af
Hyungu Kim <kimhyungu@nate.com>
2023-06-09 06:09:12 +0900

    recover app.js

--- app.js
+++ app.js
@@ -3,4 +3,25 @@ git init
 git add : 문어가  stage에 올라가는것 
 git commit
 
- 
+ select * from TableA A 
+     Left Join TableB B 
+            on A.key = B.key
+
+SELECT * FROM A 
+   INNER JOIN B 
+           ON A.key = B.key;            
+SELECT * FROM A
+   RIGHT JOIN B
+           ON A.key = B.key;
+           
+SELECT * FROM A  
+    LEFT JOIN B
+           ON A.key = B.key
+        WHERE B.key IS null;
+
+
+        
+
+
+           
+
